f80fda15bfb964228baf8f1b8e0917a0090d6253
Candyzcl <2596924715@qq.com>
2020-07-27 09:20:27 +0800

    new

---
+++ src/network/home.js
@@ -0,0 +1,13 @@
+import {request} from './request'
+
+export function getHomeMultiData(){
+  return request({
+    url:'/home/multidata'
+  })
+}
+
+
+
+
+
+
---
+++ src/views/home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <swiper>
+      <swiper-item >
+        <a :href="item.link">
+
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
+import {Swiper,SwiperItem} from 'components/common/swiper'
+
+
+export default {
+  name:"Home",
+  components:{
+      NavBar,
+      Swiper,
+      SwiperItem
+  },
+  data(){
+    return {
+      banners:[],
+      recommends:[]
+    }
+  },
+  created(){
+    //1.请求多个数据
+    getHomeMultidata().then(res=>{
+      //this.result=res;
+      this.banners=res.data.banner.list;
+      this.recommends=res.data.recommend.list
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .home-nav{
+      background-color:var(--color-tint);
+      color: #fff;
+      font-size: 20px
+  }
+</style>
